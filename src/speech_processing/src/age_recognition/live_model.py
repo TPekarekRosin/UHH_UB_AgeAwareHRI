841a0df933fd88834b84d63ab0be2d467b4c3f6f
Theresa Pekarek Rosin <theresa.pekarek-rosin@uni-hamburg.de>
2022-11-16 16:39:51 +0100

    small reformat to prepare for age recognition model

--- src/speech_processing/src/age_recognition/live_model.py
+++ src/speech_processing/src/age_recognition/live_model.py
@@ -21,37 +21,6 @@ sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
 
-class LiveInference:
-    def __init__(self, config):
-        # todo: add functionality to load own model
-        self.processor = Wav2Vec2Processor.from_pretrained(config['model_name'])
-        self.model = Wav2Vec2ForCTC.from_pretrained(config['model_name'])
-        
-        # todo add ar_model
-        # self.ar_model =
-
-    def buffer_to_text(self, audio_buffer):
-        if len(audio_buffer) == 0:
-            return ""
-
-        inputs = self.processor(torch.tensor(audio_buffer), sampling_rate=16_000, return_tensors="pt", padding=True)
-
-        with torch.no_grad():
-            logits = self.model(inputs.input_values).logits
-
-        predicted_ids = torch.argmax(logits, dim=-1)
-        transcription = self.processor.batch_decode(predicted_ids)[0]
-
-        # todo: age estimation
-        age_estimation = 0.8
-        return transcription.lower(), age_estimation
-
-    def file_to_text(self, filename):
-        audio_input, samplerate = sf.read(filename)
-        assert samplerate == 16000
-        return self.buffer_to_text(audio_input)
-
-
 class ASRLiveModel:
     exit_event = threading.Event()
 
@@ -168,5 +137,37 @@ class ASRLiveModel:
         best_match = min_levenshtein(tokens, self.tokens_list)
         distance = levenshtein(tokens, self.tokens_list[best_match])
         confidence = 1.0 - min(1.0, float(distance) / len(self.tokens_list[best_match]))
-        
+
         return self.sentence_list[best_match], confidence
+
+
+class LiveInference:
+    def __init__(self, config):
+        # todo: add functionality to load own model
+        self.processor = Wav2Vec2Processor.from_pretrained(config['model_name'])
+        self.model = Wav2Vec2ForCTC.from_pretrained(config['model_name'])
+        
+        # todo add ar_model
+        # self.ar_model =
+
+    def buffer_to_text(self, audio_buffer):
+        if len(audio_buffer) == 0:
+            return ""
+
+        inputs = self.processor(torch.tensor(audio_buffer), sampling_rate=16_000, return_tensors="pt", padding=True)
+
+        with torch.no_grad():
+            logits = self.model(inputs.input_values).logits
+
+        predicted_ids = torch.argmax(logits, dim=-1)
+        transcription = self.processor.batch_decode(predicted_ids)[0]
+
+        # todo: age estimation
+        age_estimation = 0.8
+        return transcription.lower(), age_estimation
+
+    def file_to_text(self, filename):
+        audio_input, samplerate = sf.read(filename)
+        assert samplerate == 16000
+        return self.buffer_to_text(audio_input)
+
