8daeaaacd2eddaa40ff9c0fadbc6b8da4d44e5db
Theresa Pekarek Rosin <theresa.pekarek-rosin@uni-hamburg.de>
2024-06-04 09:58:10 +0200

    replaced age_recognition models

--- src/speech_processing/src/age_recognition/age_recognition_model.py
+++ src/speech_processing/src/age_recognition/age_recognition_model.py
@@ -1,78 +1,138 @@
 import torch
 from torch import nn
+from torch.nn import CrossEntropyLoss
 from torchvision.transforms import Normalize
 import torchaudio.transforms as T
+from typing import Optional, Tuple, Union
 import logging
+from transformers import WhisperFeatureExtractor, WhisperPreTrainedModel, WhisperConfig
+from transformers.models.whisper.modeling_whisper import WhisperEncoder, WhisperDecoderLayer
 
 from .ar_components import ModifiedXVector
+from .utils import SequenceClassifierOutput
+from .attention_network import MHAClassifier, MultiHeadAttentionLayer
 
 logger = logging.getLogger('Age-Estimator-Log')
+_HIDDEN_STATES_START_POSITION = 1
 
 
 class AgeEstimation(nn.Module):
     def __init__(self, config):
-        super(AgeEstimation, self).__init__()
-        self.speaker_embeds = SpeakerEmbeddingExtractor(config)
-        self.estimator = AgeEstimationClassification(config)
-
-    def forward(self, waveform):
-        embedding = self.speaker_embeds(waveform)
-        out = self.estimator(embedding)
-        return out
-
-
-class SpeakerEmbeddingExtractor(nn.Module):
-    def __init__(self, config):
-        super(SpeakerEmbeddingExtractor, self).__init__()
-        self.feature_extraction = T.MFCC(sample_rate=config['sample_rate'],
-                                         n_mfcc=config['n_mfcc'],
-                                         melkwargs={'win_length': config['win_length'],
-                                                    'hop_length': config['hop_length']})
-        # self.batch_norm = nn.BatchNorm1d(num_features=config['model']['n_mfcc'])
-        self.x_vector = ModifiedXVector(input_dim=config['n_mfcc'])
-        self.fc1 = nn.Linear(1024, config['fc_hidden_dim'])
+        super().__init__()
         self.config = config
-        self.device = torch.device(
-            "cuda:" + str(self.config['gpu']) if torch.cuda.is_available() else "cpu")
-    
-        # for lmcl phase 2 :
-        self.nllloss = nn.CrossEntropyLoss()
-        self.fc2 = nn.Linear(config['fc_hidden_dim'], config['fc_output_dim'])
+        whisper_config = WhisperConfig.from_pretrained("openai/whisper-small",
+                                                       num_labels=self.config['model']['num_classes'])
+        whisper_config.num_classification_layers = self.config['num_classification_layers']
+        whisper_config.num_classifier_attention_heads = self.config['num_classifier_attention_heads']
+
+        self.feature_extractor = WhisperFeatureExtractor.from_pretrained()
+        self.classification_model = ClassificationWhisperModel.from_pretrained(self.config['ar_model'],
+                                                                              config=whisper_config)
 
     def forward(self, waveform):
-        mfcc = self.feature_extraction(waveform)  # [0])
-        mfcc = mfcc.unsqueeze(0)
-        norm = Normalize(mean=0, std=1)
-        mfcc_norm = norm(mfcc)
-        mfcc_norm_t = mfcc_norm.transpose(1, 2)
-    
-        pooled_out = self.x_vector(mfcc_norm_t)
-        fc1_out = self.fc1(pooled_out)
-        out = self.fc2(fc1_out)
-        
-        return out
+        input_features = self.feature_extractor(waveform, sampling_rate=16000)
+        outputs = self.classification_model(input_features)
+        return outputs.logits
 
 
-class AgeEstimationClassification(nn.Module):
+class ClassificationWhisperModel(WhisperPreTrainedModel):
     def __init__(self, config):
-        super(AgeEstimationClassification, self).__init__()
-        self.linear1 = nn.Linear(config['fc_output_dim'],
-                                 config['fc_output_dim'])
-        self.linear2 = nn.Linear(config['fc_output_dim'],
-                                 config['fc_output_dim'] // 2)
-        self.linear3 = nn.Linear(config['fc_output_dim'] // 2,
-                                 config['num_classes'])
+        super().__init__(config)
 
-        self.activation = nn.LeakyReLU()
+        self.encoder = WhisperEncoder(config)
+        num_layers = config.num_hidden_layers + 1  # transformer layers + input embeddings
+        if config.use_weighted_layer_sum:
+            self.layer_weights = nn.Parameter(torch.ones(num_layers) / num_layers)
 
-        # returns class log probabilities
-        self.log_softmax = nn.LogSoftmax(dim=1)
-    
-    def forward(self, x):
-        linear1_out = self.linear1(x)
-        linear2_out = self.linear2(linear1_out)
-        linear3_out = self.linear3(linear2_out)
-        activated_out = self.activation(linear3_out)
-        log_probs = self.log_softmax(activated_out)
-        
-        return log_probs
+        self.projector = MHAClassifier(MultiHeadAttentionLayer(config.hidden_size,
+                                                               num_heads=config.num_classifier_attention_heads),
+                                       nn.Linear(config.hidden_size, config.classifier_proj_size), config)
+        self.classifier = nn.Linear(config.classifier_proj_size, config.num_labels)
+
+        # save config separately for forward funtion
+        self.classification_config = config
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def freeze_encoder(self):
+        """
+        Calling this function will disable the gradient computation for the Whisper encoder so that its parameters will
+        not be updated during training. Only the projection layers and classification head will be updated.
+        """
+        self.encoder._freeze_parameters()
+
+    def get_input_embeddings(self) -> nn.Module:
+        return self.encoder.get_input_embeddings()
+
+    def set_input_embeddings(self, value: nn.Module):
+        self.encoder.set_input_embeddings(value)
+
+    def forward(
+            self,
+            input_features: Optional[torch.LongTensor] = None,
+            head_mask: Optional[torch.Tensor] = None,
+            encoder_outputs: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
+            labels: Optional[torch.LongTensor] = None,
+            output_attentions: Optional[bool] = None,
+            output_hidden_states: Optional[bool] = None,
+            return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], SequenceClassifierOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        ```"""
+
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        if self.config.use_weighted_layer_sum:
+            output_hidden_states = True
+        elif output_hidden_states is None:
+            output_hidden_states = self.config.output_hidden_states
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if encoder_outputs is None:
+            encoder_outputs = self.encoder(
+                input_features,
+                head_mask=head_mask,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+
+        if self.config.use_weighted_layer_sum:
+            hidden_states = encoder_outputs[_HIDDEN_STATES_START_POSITION]
+            hidden_states = torch.stack(hidden_states, dim=1)
+            norm_weights = nn.functional.softmax(self.layer_weights, dim=-1)
+            hidden_states = (hidden_states * norm_weights.view(-1, 1, 1)).sum(dim=1)
+        else:
+            hidden_states = encoder_outputs[0]
+
+        hidden_states = self.projector(hidden_states)
+        pooled_output = hidden_states.mean(dim=1)
+
+        logits = self.classifier(pooled_output)
+
+        loss = None
+
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            # move labels to correct device to enable PP
+            labels = labels.to(logits.device)
+            loss = loss_fct(logits.view(-1, self.config.num_labels), labels.view(-1))
+
+        if not return_dict:
+            output = (logits,) + encoder_outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=encoder_outputs.hidden_states,
+            attentions=encoder_outputs.attentions,
+        )
